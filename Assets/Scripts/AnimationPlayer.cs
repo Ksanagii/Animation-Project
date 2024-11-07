ece2a00cbfd9874c99f1310db12aa6d7cd908375
Luis Felipe <sluisfelipe20@outlook.com>
2024-11-07 12:13:45 -0300

    v0.001

--- Assets/Scripts/AnimationPlayer.cs
+++ Assets/Scripts/AnimationPlayer.cs
@@ -2,25 +2,44 @@ using UnityEngine;
 
 public class AnimationPlayer : MonoBehaviour
 {
-    [SerializeField] Animator animPlayer;
+    [SerializeField] private Animator animPlayer;
+    [SerializeField] private MovementPlayer move;
+    private bool estaNoChao = true;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animPlayer = GetComponent<Animator>();
+        move = GetComponent<MovementPlayer>();
     }
 
-    void Update()
+    // Update is called once per frame
+    void FixedUpdate()
     {
+        //Andando para frente
         if (Input.GetKey(KeyCode.W))
         {
             animPlayer.SetBool("Andar", true);
+            move.Andar();
         }
         else
         {
             animPlayer.SetBool("Andar", false);
         }
 
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        //Andando para trás
+        if (Input.GetKey(KeyCode.S))
+        {
+            animPlayer.SetBool("AndarTras", true);
+            move.Andar();
+        }
+        else
+        {
+            animPlayer.SetBool("AndarTras", false);
+        }
+
+        //Correndo
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
         {
             animPlayer.SetBool("Correr", true);
         }
@@ -29,45 +48,68 @@ public class AnimationPlayer : MonoBehaviour
             animPlayer.SetBool("Correr", false);
         }
 
-
-        if (Input.GetKey(KeyCode.S))
+        //Torcida
+        if(Input.GetKey(KeyCode.Q))
         {
-            animPlayer.SetBool("AndarTras", true);
-        }
-        else
-        {
-            animPlayer.SetBool("AndarTras", false);
+            animPlayer.SetTrigger("Torcida");
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            animPlayer.SetTrigger("Pular");
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
+        //Interagindo
+        if (Input.GetKey(KeyCode.E))
         {
             animPlayer.SetTrigger("Interact");
         }
 
-        if (Input.GetKeyDown(KeyCode.F))
+        //Pegando
+        if (Input.GetKey(KeyCode.G))
         {
             animPlayer.SetTrigger("Pegar");
         }
 
+        //Atacando
         if (Input.GetMouseButtonDown(0))
         {
             animPlayer.SetTrigger("Ataque");
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            move.Virar();
+            animPlayer.SetBool("Andar", true);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && estaNoChao)
+        {
+            animPlayer.SetTrigger("Pular");
+            move.Pular();
+            estaNoChao = false;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Chao"))
+        if(collision.gameObject.CompareTag("Chao"))
+        {
             animPlayer.SetBool("NoChao", true);
+            estaNoChao = true;
+        }
+        /*
+        else
+        {
+            animPlayer.SetBool("NoChao", false);
+        }
+        */
     }
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Chao"))
+        {
             animPlayer.SetBool("NoChao", false);
+            estaNoChao = false;
+        }
     }
 }
